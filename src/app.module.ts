ed7b83cedb7034769737cf9c6b9a4cf4f665920b
t2 <oo_ri@naver.com>
2022-09-13 00:00:58 +0900

    NestJS Pino Install and Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), LoggerModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
