0f2b81ed352284f2a31caae2aba3a9ce1a38f891
t2 <oo_ri@naver.com>
2022-09-12 20:51:17 +0900

    NestJS Configuration Refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 import configuration from './config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration], isGlobal: true })],
+  imports: [ConfigModule.forRoot({ load: [configuration] })],
   controllers: [AppController],
-  providers: [ConfigService, AppService],
+  providers: [AppService],
 })
 export class AppModule {}
