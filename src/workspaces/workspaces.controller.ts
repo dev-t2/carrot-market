5daa1b4a09655a5bbb1d80561b1e40bfdb5e3dbb
t2 <oo_ri@naver.com>
2022-09-14 15:04:49 +0900

    Workspaces Controller Update

--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 
-@Controller('workspaces')
-export class WorkspacesController {}
+@Controller('api/workspaces')
+export class WorkspacesController {
+  @Get()
+  getMyWorkspaces() {
+    return;
+  }
+
+  @Post()
+  createWorkspace() {
+    return;
+  }
+
+  @Get(':url/members')
+  getMembers() {
+    return;
+  }
+
+  @Post(':url/members')
+  inviteMembers() {
+    return;
+  }
+
+  @Delete(':url/members/:id')
+  kickMembers() {
+    return;
+  }
+
+  @Get(':url/members/:id')
+  getMember() {
+    return;
+  }
+
+  @Get(':url/users/:id')
+  DEPRECATED_getMember() {
+    this.getMember();
+  }
+}
