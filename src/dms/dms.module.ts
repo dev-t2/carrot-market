868611a1a0bd96af63226fda066fecca3d252ff4
t2 <oo_ri@naver.com>
2022-09-14 13:54:15 +0900

    DMs Controller and Service Update

--- src/dms/dms.module.ts
+++ src/dms/dms.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { DmsController } from './dms.controller';
+import { DmsService } from './dms.service';
+
+@Module({
+  controllers: [DmsController],
+  providers: [DmsService],
+})
 export class DmsModule {}
