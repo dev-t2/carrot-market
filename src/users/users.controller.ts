3240bbc0809dd9bf64f89f7e5157e414f7429a86
t2 <oo_ri@naver.com>
2022-09-14 13:13:33 +0900

    User Controller Update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Res, Req } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return;
+  }
+
+  @Post()
+  postUsers() {
+    return;
+  }
+
+  @Post('login')
+  login() {
+    return;
+  }
+
+  @Post('logout')
+  logout(@Req() req, @Res() res) {
+    req.logout();
+    res.clearCookie('connect.sid', { httpOnly: true });
+
+    return;
+  }
+}
