b17732686811e782b7a657bb576207dc2128ab6d
t2 <oo_ri@naver.com>
2022-09-14 13:17:35 +0900

    Project Code Refactoring

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
+
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -16,7 +17,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    // eslint-disable-next-line prettier/prettier
     return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
 });
