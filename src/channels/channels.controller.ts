7cf4e9081008f22426927eb870f20e330cea9cfe
t2 <oo_ri@naver.com>
2022-09-14 14:51:01 +0900

    Channels Controller and DTO Update

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
-@Controller('channels')
-export class ChannelsController {}
+import { PostChatDto } from './dto';
+
+@Controller('api/workspaces/:url/channels')
+export class ChannelsController {
+  @Get()
+  getChaanels() {
+    return;
+  }
+
+  @Post()
+  createChannel() {
+    return;
+  }
+
+  @Get(':name')
+  getChaanel() {
+    return;
+  }
+
+  @Get(':name/chats')
+  getChats(@Param() param, @Query() query) {
+    console.log({ param, query });
+  }
+
+  @Post(':name/chats')
+  postChat(@Body() body: PostChatDto) {
+    console.log(body);
+  }
+
+  @Get(':name/members')
+  getMembers() {
+    return;
+  }
+
+  @Post(':name/members')
+  inviteMembers() {
+    return;
+  }
+}
