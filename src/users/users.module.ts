ae58b9cc5949c1ca745aadffda4c09978b61a1ba
t2 <oo_ri@naver.com>
2022-09-14 12:57:54 +0900

    User Controller and Service Update

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+})
 export class UsersModule {}
