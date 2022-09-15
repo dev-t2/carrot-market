a3056f27a3a93d3a69bd5e839959848450f448c4
t2 <oo_ri@naver.com>
2022-09-15 15:31:59 +0900

    NestJS Config Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,52 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    LoggerModule.forRoot({
-      pinoHttp: {
-        name: process.env.NODE_ENV ?? 'development',
-        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
-        transport:
-          process.env.NODE_ENV !== 'production'
-            ? { target: 'pino-pretty', options: { levelFirst: true, singleLine: true } }
-            : undefined,
+    ConfigModule.forRoot({ isGlobal: true }),
+    LoggerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const isDevelopment = configService.get('NODE_ENV') !== 'production';
+
+        const name = isDevelopment ? 'Development' : 'Production';
+        const level = isDevelopment ? 'debug' : 'info';
+        const transport = isDevelopment
+          ? { target: 'pino-pretty', options: { levelFirst: true, singleLine: true } }
+          : undefined;
+
+        return {
+          pinoHttp: { name, level, transport },
+        };
       },
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password1234',
-      database: 'slack',
-      entities: [],
-      synchronize: process.env.NODE_ENV !== 'production',
-      logging: process.env.NODE_ENV !== 'production',
-      keepConnectionAlive: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const isDevelopment = configService.get('NODE_ENV') !== 'production';
+
+        return {
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [],
+          synchronize: isDevelopment,
+          keepConnectionAlive: isDevelopment,
+          logging: isDevelopment,
+        };
+      },
     }),
     UsersModule,
     WorkspacesModule,
