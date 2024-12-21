bebfa1da1b3efb34b17e8d2dfea167795159432b
dev-t2 <oo_ri@naver.com>
2024-12-22 07:06:08 +0900

    Carrot Market Repository Init

--- ch02/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Counter } from './counter';
-export { default as Heading } from './heading';
-export { default as ItemList } from './item-list';
-export { default as Welcome } from './welcome';
--- ch02/components/welcome.tsx
+++
@@ -1,8 +0,0 @@
-export default function Welcome({
-  name,
-}: Readonly<{
-  name: string;
-  age?: number;
-}>) {
-  return <h2>Hello, {name}</h2>;
-}
